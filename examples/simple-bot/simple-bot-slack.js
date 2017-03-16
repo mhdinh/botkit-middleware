8c26eb9541387e96a73ff6070e4c7e5ad3f0237b
mhdinh <mhdinh@us.ibm.com>
2017-03-15 18:26:46 -0700

    Update simple-bot-slack.js

--- examples/simple-bot/simple-bot-slack.js
+++ examples/simple-bot/simple-bot-slack.js
@@ -15,8 +15,8 @@
  */
 
 
-require('dotenv').config({path: '/examples/simple-bot/.env'});
-//require('dotenv').load();
+//require('dotenv').config({path: '/examples/simple-bot/.env'});
+require('dotenv').load();
 
 var Botkit = require('botkit');
 var express = require('express');
